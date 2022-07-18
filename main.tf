dc71d89b83ddb6a5027c8b34e27ce8c4b75ea6e0
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 22:21:03 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,3 @@ terraform {
     token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
   }
 }
-  resource "google_storage_bucket" "bucket" {
-    name     = "test-bucket-random-001122"
-    location = "EU"
-  }
-  resource "google_storage_bucket" "gcs_bucket" {
-    name     = "test-bucket-random-001123"
-    location = "EU"
-  }
