a1dccc8af6ec92d598aa161cc98e59cce94625b4
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 22:10:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,21 @@ resource "google_storage_bucket" "gcs_bucket" {
   name     = "test-bucket-random-001123"
   location = "EU"
 }
+
+terraform {
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.29"
+    }
+  }
+
+  cloud {
+    organization = "iavirvarei"
+  workspaces {
+      name = "DevTest"
+    }
+  token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
+  }
+}
