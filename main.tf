b34b9b4336a4d38d0126b02baa008fe2e5844d15
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-18 22:18:53 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,19 @@ terraform {
       version = ">= 3.29"
     }
   }
-
   cloud {
     organization = "iavirvarei"
-  workspaces {
-    name = "DevTest"
+    workspaces {
+      name = "DevTest"
     }
-  token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
+    token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
   }
-}   
+}
   resource "google_storage_bucket" "bucket" {
     name     = "test-bucket-random-001122"
     location = "EU"
-}
-
+  }
   resource "google_storage_bucket" "gcs_bucket" {
     name     = "test-bucket-random-001123"
     location = "EU"
-}
+  }
