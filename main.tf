fc82d88ba9e620379bc42d63ae954d814a6be48e
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-19 22:28:43 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,9 +26,9 @@ resource "google_compute_instance" "default" {
   }
 
   // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
+  //scratch_disk {
+  //  interface = "SCSI"
+ // }
 
   network_interface {
     network = "default"
