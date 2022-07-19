f3c1251363e403262d02e8d79e9b9bc0198c49b6
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-19 11:14:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
     token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
   }
 }
-resource "google_storage_bucket" "static-site" {
-  name     = "appengine-static-content"
-  location = "EU"
-}
+  resource "google_storage_bucket" "static-site" {
+    name     = "appengine-static-content"
+    location = "EU"
+  }
