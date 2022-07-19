136d01348785c970a2548a6282a1b458336c06b3
chelesteanu-deloitte <107125308+chelesteanu-deloitte@users.noreply.github.com>
2022-07-19 15:57:08 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,4 +1,6 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+  credentials = var.GOOGLE_CREDENTIALS
 }
