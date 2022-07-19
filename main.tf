e9398a8267567a13e86571a0dc43008be5178d9e
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-19 11:24:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,15 @@ terraform {
     token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
   }
 }
-  resource "google_storage_bucket" "static-site" {
-    name     = "appengine-static-content"
-    location = "EU"
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
   }
+  direction     = "INGRESS"
+  network       = "default"
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
