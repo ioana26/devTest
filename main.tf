a038a80b3faab17c751e4bc0dcd6ae881f56089a
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-19 11:51:24 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,19 +10,5 @@ terraform {
     workspaces {
       name = "devTest2"
     }
-    token = "cABF9S0BN7gaqQ.atlasv1.Xh43DVWXFiOUkmJlayYvirSXT3SuJvX1VNKFfUXq54tjulCnnibyxRrch4NsjexHtyE"
   }
-}
-
-resource "google_compute_firewall" "ssh" {
-  name = "allow-ssh"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = "default"
-  priority      = 1000
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh"]
 }
