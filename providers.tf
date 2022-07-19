cda2f1d439f04f1b08796815340f5064a28c5766
ioana26 <36165310+ioana26@users.noreply.github.com>
2022-07-19 17:19:22 +0300

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
 }
